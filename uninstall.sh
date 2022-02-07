60ff2b5736f3b8243bad009e1ed184b7edc6e798
Andrew Allbright <aallbrig@gmail.com>
2022-02-07 17:44:52 -0500

    This developer comment will surely degrade over time

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+# Uninstall script is meant to reverse the order of the install script
+# Currently, that is the responsibility of the developer/s working on the
+# project.
 
 echo "🦉 Uninstall script"
