24103fd16de57a9aad511ea8cb70e7246b4e9376
Andrew Allbright <aallbrig@gmail.com>
2022-02-07 17:44:52 -0500

    This developer comment will surely degrade over time

--- admin_uninstall.sh
+++ admin_uninstall.sh
@@ -1,2 +1,6 @@
 #!/usr/bin/env bash
+# Uninstall script is meant to reverse the order of the install script
+# Currently, that is the responsibility of the developer/s working on the
+# project.
+
 echo "🦉🚨 Admin uninstall script"
