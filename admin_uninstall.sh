f318da5a657e72fcf3545e5a87109fb153640f4a
Andrew Allbright <aallbrig@gmail.com>
2024-10-16 09:25:34 -0400

    Not needed anymore

--- admin_uninstall.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Uninstall script is meant to reverse the order of the install script
-# Currently, that is the responsibility of the developer/s working on the
-# project.
-
-echo "🦉🚨 Admin uninstall script"
